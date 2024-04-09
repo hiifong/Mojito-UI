e4f176f58874f0c9fbc99a26ed9aac8c1f26f081
hiifong <i@hiif.ong>
2024-04-09 11:15:04 +0800

    Update

--- src/components/comment/Comment.vue
+++ src/components/comment/Comment.vue
@@ -6,18 +6,11 @@ import { UToast } from 'undraw-ui'
 import { getComment, createComment } from '@/api/comment'
 import { ElMessage } from 'element-plus'
 import { useRoute } from 'vue-router'
+import dayjs from 'dayjs'
 
 const route = useRoute()
 const user = useUserStore().user
 const repo = inject('repo')
-let form = ref({
-  content: '',
-  page: 1,
-  pageSize: 10,
-  parentID: 0,
-  repoID: 3,
-  uid: user.id
-})
 
 const config = reactive({
   user: {
@@ -34,8 +27,38 @@ const config = reactive({
   total: 10
 })
 
+let pageNum = ref(1)
+let pageSize = ref(10)
+// 是否禁用滚动加载评论
+const disable = ref(false)
+let total = ref(100)
+
+// 加载更多评论
+const more = () => {
+  console.log('more:', disable.value)
+  let form = ref({
+    page: pageNum.value,
+    pageSize: pageSize.value,
+    repoID: parseInt(route.params.id)
+  })
+  if (pageNum.value <= Math.ceil(total.value / pageSize.value)) {
+    setTimeout(() => {
+      getCommentList(form.value.repoID, form.value).then((res) => {
+        let count = res.count
+        let commentList = res.commentList
+        console.log('count: ', count)
+        console.log('commentList: ', commentList)
+        config.comments.push(commentList)
+        total.value = count
+      })
+      pageNum++
+    }, 200)
+  } else {
+    disable.value = true
+  }
+}
+
 const getCommentList = async (id = route.params.id, data) => {
-  form.value.uid = 0
   const result = await getComment(id, data).then((res) => {
     return res.data
   })
@@ -44,20 +67,60 @@ const getCommentList = async (id = route.params.id, data) => {
     ElMessage.error('获取评论失败')
     return
   }
-  form.value.uid = user.id
-  result.data.commentList.forEach((element) => {
-    console.log('ele', element)
-    element.reply = {
-      total: 10,
-      list: element.reply
-    }
+  result.data.commentList.forEach((el) => {
+    el.createTime = dayjs.unix(el.createTime)
+    el.reply = handleReply(el.reply)
+    el.user = handleUser(el.user)
+    console.log('ele', el)
   })
   console.log('list', result.data.commentList)
-  config.comments = result.data.commentList
+  let count = result.data.count
+  let commentList = result.data.commentList
+  return { count, commentList }
 }
 
+const handleReply = (replys) => {
+  let count = 0
+  let list = []
+  replys.forEach((el) => {
+    count++
+    el.createTime = dayjs.unix(el.createTime)
+    el.user = handleUser(el.user)
+    list.push(el)
+  })
+  if (count === 0) {
+    return null
+  }
+  return { count, list }
+}
+
+const handleUser = (user) => {
+  const id = user.id
+  const username = user.username
+  const avatar = user.avatar
+  const homeLink = null
+  return { id, username, avatar, homeLink }
+}
+
+// const firstPageCommentList = async () => {
+//   let form = ref({
+//     page: pageNum.value,
+//     pageSize: pageSize.value,
+//     repoID: parseInt(route.params.id)
+//   })
+//   await getCommentList(form.value.repoID, form.value).then((res) => {
+//     let count = res.count
+//     let commentList = res.commentList
+//     console.log('count: ', count)
+//     console.log('commentList: ', commentList)
+//     config.comments.push(commentList)
+//     total.value = count
+//   })
+// }
+
+// firstPageCommentList()
+
 console.log('repo id', repo.id)
-getCommentList(repo.value.id, form.value)
 
 //排序
 const latest = ref(true)
@@ -92,5 +155,7 @@ const submit = async ({ content, parentId, files, finish, reply }) => {
 </script>
 
 <template>
-  <u-comment :config="config" @submit="submit" relative-time> </u-comment>
+  <u-comment-scroll :disable="disable" @more="more">
+    <u-comment :config="config" @submit="submit" relative-time> </u-comment>
+  </u-comment-scroll>
 </template>
