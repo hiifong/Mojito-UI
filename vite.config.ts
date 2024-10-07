4436fa404efaeff1dc490dc49e1353717e5e73ba
hiifong <i@hiif.ong>
2024-10-07 18:50:20 +0800

    Fix: https://github.com/gofiber/fiber/issues/1763

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { PrimeVueResolver } from 'unplugin-vue-components/resolvers'
 import { chunkSplitPlugin } from 'vite-plugin-chunk-split'
 
+// https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+// https://datatracker.ietf.org/doc/html/rfc2396
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$%&*+_,:;<=>?[\]^`{|}\u007F]/g // Add '_' char
+// const DRIVE_LETTER_REGEX = /^[a-z]:/i
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -23,6 +29,10 @@ export default defineConfig({
             return 'images/[name].[hash].[ext]'
           }
           return '[name].[hash].[ext]'
+        },
+        sanitizeFileName(name: string): string {
+          // 去掉无效的字符
+          return name.replaceAll(INVALID_CHAR_REGEX, '')
         },
         minifyInternalExports: true
       }
